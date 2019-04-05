0b03c3094cf1090d42d31f8b492d87ff8ca8f010
Joseph Nwachukwu <jnwachukwu@TCAdmins-MacBook-Pro.local>
2019-04-05 18:36:02 -0400

    timesheets list

--- src/app/timesheets/timesheet-detail-modal/timesheet-detail-modal.component.ts
+++ src/app/timesheets/timesheet-detail-modal/timesheet-detail-modal.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+import { Timesheet } from '../shared/timesheet';
 
 @Component({
   selector: 'timesheet-detail-modal',
   templateUrl: './timesheet-detail-modal.component.html',
   styleUrls: ['./timesheet-detail-modal.component.scss']
 })
+
 export class TimesheetDetailModalComponent implements OnInit {
-	@Input() timesheetId: string;
+
+  @Output() closeModal = new EventEmitter<boolean>()
+  @Input() timesheet: Timesheet;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+
+  close = () => {
+  	this.closeModal.emit(true);
   }
 
 }
